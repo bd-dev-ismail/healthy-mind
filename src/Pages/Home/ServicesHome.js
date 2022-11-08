b305862e6f878afd0c1aa801cfc2f4f5ed17cca8
bd-dev-ismail <ismailratul7@gmail.com>
2022-11-08 18:10:15 +0600

    display only 3 services in home page

--- src/Pages/Home/ServicesHome.js
+++ src/Pages/Home/ServicesHome.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import SingleServicesHome from './SingleServicesHome';
 
 const ServicesHome = () => {
+  const [services, setServices] = useState([]);
+  console.log(services);
+  const size = 3;
+  useEffect(() => {
+    fetch(`http://localhost:5000/services?size=${size}`)
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
     return (
       <div className="container mx-auto mb-20">
         <div>
@@ -10,45 +20,20 @@ const ServicesHome = () => {
           </h3>
         </div>
         <div>
-          <div className="max-w-2xl mx-auto">
-            <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-              <a href="/">
-                <img
-                  className="rounded-t-lg"
-                  src="https://flowbite.com/docs/images/blog/image-1.jpg"
-                  alt="..."
-                />
-              </a>
-              <div className="p-5">
-                <a href="/">
-                  <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
-                    Noteworthy technology acquisitions 2021
-                  </h5>
-                </a>
-                <p className="font-normal text-gray-700 mb-3 dark:text-gray-400">
-                  Here are the biggest enterprise technology acquisitions of
-                  2021 so far, in reverse chronological order.
-                </p>
-                <a
-                  href="/"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Read more
-                  <svg
-                    className="-mr-1 ml-2 h-4 w-4"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                </a>
-              </div>
-            </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-14">
+            {services.map((service) => (
+              <SingleServicesHome
+                key={service._id}
+                service={service}
+              ></SingleServicesHome>
+            ))}
+          </div>
+          <div className="text-center">
+            <Link to="/services">
+              <button className="btn bg-color-a border-0">
+                See All Services
+              </button>
+            </Link>
           </div>
         </div>
       </div>
---
+++ src/Pages/Home/SingleServicesHome.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+
+const SingleServicesHome = ({ service }) => {
+    const { bannerURL, price, name, desc } = service;
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <PhotoProvider>
+        <PhotoView src={bannerURL}>
+          <figure>
+            <img src={bannerURL} alt="Shoes" className="h-64 cursor-pointer" />
+          </figure>
+        </PhotoView>
+      </PhotoProvider>
+      <div className="card-body bg-color-a text-white">
+        <h2 className="card-title">{name}</h2>
+        <p>Fees: ${price}</p>
+        <p>{`${desc.slice(0, 100)}...`}</p>
+        <div className="card-actions justify-end">
+          <div className="btn btn-sm bg-color-b border-0">See Details</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleServicesHome;
