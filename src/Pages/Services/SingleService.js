8289e619146dbf8b3afba5e03889725646e43f9d
bd-dev-ismail <ismailratul7@gmail.com>
2022-11-08 17:54:49 +0600

    click to display image by react photo view

--- src/Pages/Services/SingleService.js
+++ src/Pages/Services/SingleService.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 const SingleService = ({ service }) => {
     const {bannerURL, price, name, desc} = service;
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
-      <figure>
-        <img src={bannerURL} alt="Shoes" className="h-64" />
-      </figure>
+      <PhotoProvider>
+        <PhotoView src={bannerURL}>
+          <figure>
+            <img src={bannerURL} alt="Shoes" className="h-64 cursor-pointer" />
+          </figure>
+        </PhotoView>
+      </PhotoProvider>
       <div className="card-body bg-color-a text-white">
-        <h2 className="card-title">
-          {name}
-          {/* <div className="badge badge-secondary">asdfg</div> */}
-        </h2>
+        <h2 className="card-title">{name}</h2>
         <p>Fees: ${price}</p>
         <p>{`${desc.slice(0, 100)}...`}</p>
         <div className="card-actions justify-end">
           <div className="btn btn-sm bg-color-b border-0">See Details</div>
-         
         </div>
       </div>
     </div>
